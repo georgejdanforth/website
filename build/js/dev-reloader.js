f22738c953b5f4135e96ca000b4fb8c26ed26c29
George Danforth <georgejdanforth@gmail.com>
2025-03-22 10:20:21 -0400

    Add dev reloader script

--- build/js/dev-reloader.js
+++ build/js/dev-reloader.js
@@ -1,1 +1,24 @@
-console.log("Hello from dev-reloader.js!");
+var sha256 = null;
+
+var intervalId = setInterval(() => {
+  fetch("/sha256.txt")
+    .then(response => {
+      if (!response.ok) {
+        return null;
+      }
+      return response.text();
+    })
+    .then(text => {
+      if (text === null) {
+        console.log("No sha256.txt found, skipping check.");
+        return;
+      }
+      if (sha256 === null) {
+        sha256 = text;
+      } else if (sha256 !== text) {
+        clearInterval(intervalId);
+        console.log("Reloading...");
+        location.reload();
+      }
+  });
+}, 1000);
